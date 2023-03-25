6d0df78c5869ebc9b3cf0691f97fc5125398fc37
mrchen <shensuguo@163.com>
2023-03-25 16:09:40 +0800

    首次提交

---
+++ src/main/java/com/example/app/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.app.controller;
+
+import com.example.app.common.Result;
+import com.example.app.handler.NoAuth;
+import com.example.app.model.WXAuth;
+import com.example.app.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("user")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getSessionId")
+    @NoAuth
+    public String getSessionId(String code) {
+        return userService.getSessionId(code);
+    }
+
+    @PostMapping("/authLogin")
+    @NoAuth
+    public Result authLogin(@RequestBody WXAuth wxAuth) {
+        Result result = userService.authLogin(wxAuth);
+        log.info("{}", result);
+        return result;
+    }
+
+    @GetMapping("userinfo")
+    public Result userinfo(Boolean refresh) {
+        return userService.userinfo(refresh);
+    }
+}
